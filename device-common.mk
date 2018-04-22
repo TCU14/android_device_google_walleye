e4ae0633223a1fde9f760c60ebdf60964b69f73e
calebcabob <calphonic@gmail.com>
2018-04-22 17:51:08 -0400

    Derp: Add these back
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -67,6 +67,7 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init-$(PRODUCT_HARDWARE).rc \
     $(LOCAL_PATH)/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).usb.rc \
+    $(LOCAL_PATH)/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg
 
 # Input device files
 PRODUCT_COPY_FILES += \
